5167f9e479d22e3dad8fb6ccc8a4908395426aac
arthurvenicio <stuks.midia@gmail.com>
2022-02-09 16:38:17 -0300

    Working with StreamWriter and modify and create .csv

--- CourseCSharp/Program.cs
+++ CourseCSharp/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.IO;
-using System.Globalization;
-using System.Collections.Generic;
+﻿using System.Globalization;
 using System;
-using CourseCSharp.Entities;
-using CourseCSharp.Entities.Exceptions;
+using System.IO;
 
 namespace CourseCSharp
 {
@@ -12,14 +9,31 @@ namespace CourseCSharp
         static void Main(string[] args)
         {
 
-            string sourcePath = @"c:\temp\file1.txt";
-            
+            string sourcePath = @"c:\temp\file.csv";
+            string targetPath = @"c:\temp\out\file2.csv";
 
-            Console.WriteLine(Path.GetDirectoryName(sourcePath));
-            Console.WriteLine(Path.GetFileName(sourcePath));
-            Console.WriteLine(Path.GetFileNameWithoutExtension(sourcePath));
-            Console.WriteLine(Path.GetRandomFileName());
-            Console.WriteLine(Path.GetTempPath());
+           
+            try{
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(sourcePath) + @"\out");
+
+                using(StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] texts = line.Split(",");
+                        string title = texts[0];
+                        double price = double.Parse(texts[1], CultureInfo.InvariantCulture);
+                        double quantity = double.Parse(texts[2], CultureInfo.InvariantCulture);
+                        string result = title + "," + (price * quantity).ToString("F2", CultureInfo.InvariantCulture);
+                        sw.WriteLine(result);
+                    }
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine($"An erro as ocurred: {e.Message}");
+            }
 
         }
     }
